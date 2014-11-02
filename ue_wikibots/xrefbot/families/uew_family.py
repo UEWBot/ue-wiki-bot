7174d0c940a0f2925f429de235a8ca292d3685e8
Chris <>
2014-11-02 21:50:53 +0000

    Sort namespace alterations

--- ue_wikibots/xrefbot/families/uew_family.py
+++ ue_wikibots/xrefbot/families/uew_family.py
@@ -8,25 +8,23 @@ class Family(wikia_family.Family):
         self.langs = {'en': u'underworld-empire.wikia.com'}
         self.namespaces[4] = { '_default': 'Underworld Empire Wiki', }
         self.namespaces[5] = { '_default': 'Underworld Empire Wiki talk', }
+        del(self.namespaces[100]['_default'])
+        del(self.namespaces[101]['_default'])
         self.namespaces[110] = { '_default': u'Forum', }
         self.namespaces[111] = { '_default': u'Forum talk', }
+        del(self.namespaces[112]['_default'])
+        del(self.namespaces[113]['_default'])
         self.namespaces[500] = { '_default': u'User blog', }
         self.namespaces[501] = { '_default': u'User blog comment', }
         self.namespaces[502] = { '_default': u'Blog', }
         self.namespaces[503] = { '_default': u'Blog talk', }
-        self.namespaces[1200] = { '_default': u'Message Wall', }
-        self.namespaces[1201] = { '_default': u'Thread', }
-        self.namespaces[1202] = { '_default': u'Message Wall Greeting', }
-        self.namespaces[112] = { '_default': u'Mini', }
-        self.namespaces[113] = { '_default': u'Mini talk', }
         self.namespaces[700] = { '_default': u'Top 10 list', }
         self.namespaces[701] = { '_default': u'Top 10 list talk', }
         self.namespaces[828] = { '_default': u'Module', }
         self.namespaces[829] = { '_default': u'Module talk', }
-        del(self.namespaces[100]['_default'])
-        del(self.namespaces[101]['_default'])
-        del(self.namespaces[112]['_default'])
-        del(self.namespaces[113]['_default'])
+        self.namespaces[1200] = { '_default': u'Message Wall', }
+        self.namespaces[1201] = { '_default': u'Thread', }
+        self.namespaces[1202] = { '_default': u'Message Wall Greeting', }
 
     def path(self, code):
         return '/index.php'

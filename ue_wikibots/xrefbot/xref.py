f13f25913bc951e2fd382c5fa527c66c878e4a38
Chris Brand <UEWBot@users.noreply.github.com>
2015-03-19 12:55:27 -0700

    Update copyright dates
    All these files were also modified in 2015.

--- ue_wikibots/xrefbot/xref.py
+++ ue_wikibots/xrefbot/xref.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2013-2014 Chris Brand
+# Copyright (C) 2013-2015 Chris Brand
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

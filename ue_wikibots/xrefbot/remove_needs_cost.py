156ad265f63dc26dd27cbaa47b333f23a75a2068
Chris Brand <UEWBot@users.noreply.github.com>
2017-08-03 14:57:27 -0700

    Add some more scripts
    Even though they are unlikely to be needed again

---
+++ ue_wikibots/xrefbot/remove_needs_cost.py
@@ -0,0 +1,109 @@
+# Copyright (C) 2014 Chris Brand
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#! /usr/bin/python
+
+"""
+Script to remove the Needs Cost category from Special Item pages on Underworld Empire Wiki
+"""
+
+from __future__ import absolute_import
+import sys
+import os
+import operator
+from six.moves import range
+sys.path.append(os.environ['HOME'] + '/ue/ue_wikibots/core')
+
+import pywikibot
+from pywikibot import pagegenerators
+import re
+import difflib
+
+# Summary message when using this module as a stand-alone script
+summary = u'Robot: Remove from Needs Cost category'
+
+CATEGORY_RE_STR = ur'\[\[\s*Category:\s*%s\s*\]\]'
+
+class ItemBot:
+    def __init__(self, generator, acceptall = False):
+        self.generator = generator
+        self.acceptall = acceptall
+
+    # This is copied from xref.py
+    def _remove_category(self, text, category):
+        """
+        Return the text with the appropriate category removed.
+
+        text -- current page text.
+        category -- the name of the category itself.
+
+        Return the new page text.
+        """
+        Rcat = re.compile(CATEGORY_RE_STR % category)
+        # Remove the category
+        return Rcat.sub('', text)
+
+    def update_or_create_page(self, old_page, new_text):
+        """
+        Reads the current text of page old_page,
+        compare it with new_text, prompts the user,
+        and uploads the page
+        """
+        # Read the original content
+        old_text = old_page.get()
+        # Give the user some context
+        if old_text != new_text:
+            pywikibot.output(new_text)
+        pywikibot.showDiff(old_text, new_text)
+        # Get a decision
+        prompt = u'Modify this page ?'
+        # Did anything change ?
+        if old_text == new_text:
+            pywikibot.output(u'No changes necessary to %s' % old_page.title());
+        else:
+            if not self.acceptall:
+                choice = pywikibot.input_choice(u'Do you want to accept these changes?',  [('Yes', 'Y'), ('No', 'n'), ('All', 'a')], 'N')
+                if choice == 'a':
+                    self.acceptall = True
+            if self.acceptall or choice == 'y':
+                # Write out the new version
+                old_page.put(new_text, summary)
+
+    def update_lts(self):
+        """
+        Creates or updates each page in the Special Items category.
+        """
+        # Update every page in the Special Items category
+        cat = pywikibot.Category(pywikibot.Site(), u'Special Items')
+
+        for page in list(cat.articles(recurse=False)):
+            text = page.get()
+            text = self._remove_category(text, u'Needs Cost')
+            # Update the page
+            pywikibot.output(u"\n\n>>> \03{lightpurple}%s\03{default} <<<" % page.title())
+            self.update_or_create_page(page, text);
+
+    def run(self):
+        self.update_lts()
+
+def main():
+    bot = ItemBot(None)
+    bot.run()
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        pywikibot.stopme()
+
---
+++ ue_wikibots/xrefbot/use_boss_epicthresholds_template.py
@@ -0,0 +1,117 @@
+# Copyright (C) 2014 Chris Brand
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#! /usr/bin/python
+
+"""
+Script to use the BossEpicThresholds template in all Bosses
+"""
+
+from __future__ import absolute_import
+from __future__ import print_function
+import sys
+import os
+import operator
+sys.path.append(os.environ['HOME'] + '/ue/ue_wikibots/core')
+
+import pywikibot
+import difflib
+import utils
+
+# Summary message when using this module as a stand-alone script
+summary = u'Robot: Use BossSpeedkill template'
+
+class DropBot:
+
+    """Class to insert the BossSpeedkill template on every page."""
+
+    def __init__(self, acceptall = False):
+        """
+        Class constructor.
+
+        acceptall -- pass True to not ask for user confirmation before
+                     updating pages.
+        """
+        self.acceptall = acceptall
+
+    def treat(self, page):
+        """
+        Check the page, and update if necessary.
+
+        page -- Page to check.
+        """
+        try:
+            # Show the title of the page we're working on.
+            # Highlight the title in purple.
+            pywikibot.output(u"\n\n>>> \03{lightpurple}%s\03{default} <<<" % page.title())
+            text = page.get()
+            old_text = text
+            text = text.replace(u'#', u'{{BossEpicThresholds|1 Epic:=', 1)
+            text = text.replace(u'#', u'|2 Epics:=', 1)
+            text = text.replace(u'#', u'|3 Epics:=', 1)
+            tmp_text = text
+	    text = text.replace(u'\n==={{Legendary}} Rewards', u'}}\n==={{Legendary}} Rewards')
+            if tmp_text == text:
+	        text = text.replace(u'\n==={{Epic}} Rewards', u'}}\n==={{Epic}} Rewards')
+            text = text.replace(u' points',u'')
+            # Give the user some context
+            pywikibot.showDiff(old_text, text)
+            # TODO Modify to treat just whitespace as unchanged
+            # Just comparing text with page.get() wasn't sufficient
+            changes = False
+            for diffline in difflib.ndiff(page.get().splitlines(),
+                                          text.splitlines()):
+                if not diffline.startswith(u'  '):
+                    changes = True
+                    break
+            if changes:
+                if not self.acceptall:
+                    choice = pywikibot.input_choice(u'Do you want to accept these changes?',
+                                                    [('Yes', 'Y'),
+                                                     ('No', 'n'),
+                                                     ('All', 'a')],
+                                                    'N')
+                    if choice == 'a':
+                        self.acceptall = True
+                if self.acceptall or choice == 'y':
+                    page.put(text, summary)
+            else:
+                pywikibot.output('No changes were necessary in %s' % page.title())
+        except pywikibot.NoPage:
+            pywikibot.output("Page %s does not exist?!" % page.title(asLink=True))
+        except pywikibot.IsRedirectPage:
+            pywikibot.output("Page %s is a redirect; skipping." % page.title(asLink=True))
+        except pywikibot.LockedPage:
+            pywikibot.output("Page %s is locked?!" % page.title(asLink=True))
+
+    def run(self):
+        """Call treat() for every Boss page."""
+        cat = pywikibot.Category(pywikibot.Site(),
+                                 u'Category:Bosses')
+        #all_cats = set(c.title(withNamespace=False) for c in cat.subcategories(recurse=True))
+        #all_cats.append(cat)
+        pages = cat.articles(recurse=True)
+        for page in pages:
+            self.treat(page)
+
+def main():
+    bot = DropBot()
+    bot.run()
+
+if __name__ == "__main__":
+    try:
+        main()
+    finally:
+        pywikibot.stopme()
+

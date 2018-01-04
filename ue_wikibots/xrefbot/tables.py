617654bf8970f53b861f9947d02a2fedb9c74690
Chris Brand <chris_brand@shaw.ca>
2018-01-04 11:26:22 -0800

    Tables: python2/3 compatibility

--- ue_wikibots/xrefbot/tables.py
+++ ue_wikibots/xrefbot/tables.py
@@ -39,6 +39,7 @@ Generate the following tables:
 
 from __future__ import absolute_import
 from __future__ import print_function
+from __future__ import unicode_literals
 import sys
 import os
 import operator
